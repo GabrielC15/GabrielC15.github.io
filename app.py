672255bcad5e0f45c16fa29855879e4a341565eb
AnotherElessar <104772894+AnotherElessar@users.noreply.github.com>
2023-03-16 08:21:46 +0000

    Thu, Mar 16, 2023, 4:21 PM +08:00

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
-
+if __name__ == '__main__':
+    app.run(debug=True)
+app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -18,8 +20,6 @@ def education():
 def about():
     return render_template("about.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
-app = Flask(__name__)
+
 
 
