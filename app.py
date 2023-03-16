196f37f7283608a57f5a3f0a27182012d26c3ea7
AnotherElessar <104772894+AnotherElessar@users.noreply.github.com>
2023-03-16 14:08:21 +0800

    Initial commit

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 @app.route("/projects")
 def projects():
