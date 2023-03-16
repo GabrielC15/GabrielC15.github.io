e6944a4c0c5efc4795f32d6eedb3af6ed590ad59
AnotherElessar <104772894+AnotherElessar@users.noreply.github.com>
2023-03-16 05:07:15 +0000

    Thu, Mar 16, 2023, 1:07 PM +08:00

--- flask.py
+++ flask.py
@@ -4,3 +4,19 @@ from flask_session import Session
 app = Flask(__name__)
 
 @app.route("/")
+    return render_template("index.html")
+
+@app.route("/home")
+    return render_template("home.html")
+
+@app.route("/projects")
+    return render_template("projects.html")
+
+@app.route("/education")
+    return render_template("education.html")
+
+@app.route("/about")
+    return render_template("about.html")
+
+@app.route("/resume")
+    return render_template("resume.html")
